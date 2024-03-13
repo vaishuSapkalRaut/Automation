82c35523f047c6a3d46a96cba8664b947e098eac
DELL <DELL@DESKTOP-2J91U8P>
2024-03-13 22:52:00 +0530

    removed space

--- Automation/src/DropDown/DemoAppQspider.java
+++ Automation/src/DropDown/DemoAppQspider.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
 public class DemoAppQspider {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
